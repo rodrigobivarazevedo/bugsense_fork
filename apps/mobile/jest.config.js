8d0c96dcdbefa8e40a477d30bc008d97f8120621
so-mb <somtochukwumbuko@gmail.com>
2025-04-22 17:41:21 +0200

    Add Jest configuration and setup for mobile app testing

---
+++ apps/mobile/jest.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    preset: 'jest-expo',
+    testEnvironment: 'jsdom',
+    setupFilesAfterEnv: [
+      '<rootDir>/jest-setup.ts'
+    ],
+    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+    testMatch: [
+      '<rootDir>/**/?(*.)+(spec|test).[jt]sx?(x)',
+    ],
+    transformIgnorePatterns: [
+      'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)'
+    ],
+    moduleNameMapper: {
+      '^@/(.*)$': '<rootDir>/src/$1'
+    }
+  };
+  
