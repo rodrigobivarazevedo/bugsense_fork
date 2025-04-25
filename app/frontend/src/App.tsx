ee1b3a68ed76de4a8e3cf1d26fcdf1ba13a601e0
so-mb <somtochukwumbuko@gmail.com>
2025-04-26 01:55:50 +0200

    Update App component to replace LandingScreen with Home and add navigation for Scan, Discover, Results, and Menu screens

--- app/frontend/src/App.tsx
+++ app/frontend/src/App.tsx
@@ -4,11 +4,16 @@ import { I18nextProvider } from 'react-i18next';
 import i18n from './translations/i18n';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { LandingScreen } from './screens/landing/LandingScreen';
 import HeaderBar from './components/HeaderBar';
 import BottomBar from './components/BottomBar';
 import styled from 'styled-components/native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+
+import Home from './screens/Home';
+import Scan from './screens/Scan';
+import Discover from './screens/Discover';
+import Results from './screens/Results';
+import Menu from './screens/Menu';
 
 const Stack = createNativeStackNavigator();
 
@@ -29,7 +34,11 @@ export default function App() {
             <HeaderBar />
             <ContentContainer>
               <Stack.Navigator screenOptions={{ headerShown: false }}>
-                <Stack.Screen name="Landing" component={LandingScreen} />
+                <Stack.Screen name="Home" component={Home} />
+                <Stack.Screen name="Scan" component={Scan} />
+                <Stack.Screen name="Discover" component={Discover} />
+                <Stack.Screen name="Results" component={Results} />
+                <Stack.Screen name="Menu" component={Menu} />
               </Stack.Navigator>
             </ContentContainer>
             <BottomBar />
