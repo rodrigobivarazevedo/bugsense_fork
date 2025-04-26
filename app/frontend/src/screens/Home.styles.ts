69d113a1900e7ded617b38095add243dc868ac5c
so-mb <somtochukwumbuko@gmail.com>
2025-04-26 16:46:27 +0200

    Add Header and Grid components to Home screen with user greeting and icon boxes

--- app/frontend/src/screens/Home.styles.ts
+++ app/frontend/src/screens/Home.styles.ts
@@ -10,6 +10,7 @@ export const Root = styled.View`
   padding: ${rem(1.25)}px;
 `;
 
+// TODO: remove this after other screens are implemented
 export const Title = styled.Text`
   ${themeTypography.h1};
   color: ${themeColors.primary};
@@ -34,4 +35,70 @@ export const ButtonText = styled.Text`
   ${themeTypography.p};
   font-weight: 600;
   color: ${themeColors.white};
+`;
+
+export const Header = styled.View`
+  width: 100%;
+  flex-direction: column;
+  align-items: flex-start;
+  margin-bottom: ${rem(2)}px;
+`;
+
+export const HeaderRow = styled.View`
+  width: 100%;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+export const Lottie = styled.View`
+  width: ${rem(12.5)}px;
+  height: ${rem(12.5)}px;
+`;
+
+export const Greeting = styled.Text`
+  ${themeTypography.p};
+  color: ${themeColors.text};
+  position: absolute;
+  bottom: ${rem(0.25)}px;
+  left: ${rem(0.25)}px;
+`;
+
+export const UserName = styled.Text`
+  ${themeTypography.h1};
+  color: ${themeColors.primary};
+  margin-bottom: ${rem(1)}px;
+`;
+
+export const Grid = styled.View`
+  width: 100%;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: space-between;
+`;
+
+export const Box = styled.TouchableOpacity`
+  width: 48%;
+  aspect-ratio: 1;
+  background-color: ${themeColors.accent};
+  border-radius: ${rem(1)}px;
+  align-items: center;
+  justify-content: center;
+  margin-bottom: ${rem(1)}px;
+  elevation: 2;
+  shadow-color: #000;
+  shadow-offset: 0px 1px;
+  shadow-opacity: 0.2;
+  shadow-radius: 1px;
+`;
+
+export const BoxIcon = styled.View`
+  margin-bottom: ${rem(0.5)}px;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const BoxLabel = styled.Text`
+  ${themeTypography.p};
+  color: ${themeColors.primary};
+  text-align: center;
 `;
--- app/frontend/src/screens/Home.tsx
+++ app/frontend/src/screens/Home.tsx
@@ -1,21 +1,76 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import * as S from './Home.styles';
+import RenderLottie from '../components/RenderLottie';
+import RenderIcon from '../components/RenderIcon';
+import { rem } from '../utils/responsive';
 
 export const Home: React.FC = () => {
   const { t } = useTranslation();
+  // TODO: get user name from API
+  const userName = 'Jane Julian Vernonica Doe';
 
   return (
     <S.Root>
-      <S.Title>
-        Welcome to the app
-      </S.Title>
-      <S.Subtitle>
-        {t('check_health_in_colors')}
-      </S.Subtitle>
-      <S.Button onPress={() => { /* navigate */ }}>
-        <S.ButtonText>{t('get_started')}</S.ButtonText>
-      </S.Button>
+      <S.Header>
+        <S.HeaderRow>
+          <S.Greeting>Hello</S.Greeting>
+          <S.Lottie>
+            <RenderLottie
+              name="homeHello"
+              startFrame={0}
+              endFrame={150}
+            />
+          </S.Lottie>
+        </S.HeaderRow>
+        <S.UserName>{userName}</S.UserName>
+      </S.Header>
+      <S.Grid>
+        <S.Box>
+          <S.BoxIcon>
+            <RenderIcon
+              family="materialCommunity"
+              icon="format-list-checkbox"
+              fontSize={rem(2.25)}
+              color="primary"
+            />
+          </S.BoxIcon>
+          <S.BoxLabel>Overviews</S.BoxLabel>
+        </S.Box>
+        <S.Box>
+          <S.BoxIcon>
+            <RenderIcon
+              family="octicons"
+              icon="light-bulb"
+              fontSize={rem(2.25)}
+              color="primary"
+            />
+          </S.BoxIcon>
+          <S.BoxLabel>Discover</S.BoxLabel>
+        </S.Box>
+        <S.Box>
+          <S.BoxIcon>
+            <RenderIcon
+              family="fontAwesome"
+              icon="newspaper-o"
+              fontSize={rem(2.25)}
+              color="primary"
+            />
+          </S.BoxIcon>
+          <S.BoxLabel>News</S.BoxLabel>
+        </S.Box>
+        <S.Box>
+          <S.BoxIcon>
+            <RenderIcon
+              family="materialCommunity"
+              icon="email-send-outline"
+              fontSize={rem(2.25)}
+              color="primary"
+            />
+          </S.BoxIcon>
+          <S.BoxLabel>Contact Us</S.BoxLabel>
+        </S.Box>
+      </S.Grid>
     </S.Root>
   );
 };
