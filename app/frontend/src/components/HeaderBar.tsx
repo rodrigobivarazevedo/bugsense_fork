a99de6ac969d74a6a38bafe9c4473d03018f4c07
so-mb <somtochukwumbuko@gmail.com>
2025-04-29 14:59:16 +0200

    Add Account screen with user profile details and navigation; update App routes and HeaderBar for new screen

--- app/frontend/src/components/HeaderBar.tsx
+++ app/frontend/src/components/HeaderBar.tsx
@@ -4,9 +4,22 @@ import Logo from './Logo';
 import * as S from './HeaderBar.styles'
 import RenderIcon from './RenderIcon';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { useNavigation } from '@react-navigation/native';
+import { NativeStackNavigationProp } from '@react-navigation/native-stack';
+
+type RootStackParamList = {
+    Home: undefined;
+    Scan: undefined;
+    Results: undefined;
+    More: undefined;
+    Account: undefined;
+};
+
+type NavigationProp = NativeStackNavigationProp<RootStackParamList>;
 
 const HeaderBar: React.FC = () => {
     const insets = useSafeAreaInsets();
+    const navigation = useNavigation<NavigationProp>();
 
     return (
         <S.Container insets={insets}>
@@ -25,7 +38,7 @@ const HeaderBar: React.FC = () => {
                         color="primary"
                     />
                 </TouchableOpacity>
-                <TouchableOpacity onPress={() => { /* Handle profile press */ }}>
+                <TouchableOpacity onPress={() => navigation.navigate('Account')}>
                     <RenderIcon
                         family="materialIcons"
                         icon="account-circle"
---
+++ app/frontend/src/screens/Account.tsx
@@ -0,0 +1,134 @@
+import React from 'react';
+import { useTranslation } from 'react-i18next';
+import * as S from './Account.styles';
+import RenderIcon from '../components/RenderIcon';
+import Logo from '../components/Logo';
+import { rem } from '../utils/responsive';
+
+export const Account: React.FC = () => {
+    const { t } = useTranslation();
+    // TODO: get user data from API
+    const userName = 'Jane Julian Vernonica Doe';
+    const dateJoined = '01.04.2022';
+    const gender = 'Female';
+    const dob = '02.02.2002';
+    const email = 'somtochukwumbuko@gmail.com';
+    const phone = '0162-7033954';
+    const address = 'Alois-Gäbl-Str. 4\n84347 Pfarrkirchen\nDeutschland';
+
+    return (
+        <S.Scroll>
+            <S.ProfileCard>
+                <S.ProfileCardBgLogo>
+                    <Logo width={120} height={120} opacity={0.07} />
+                </S.ProfileCardBgLogo>
+                <S.ProfileCardContent>
+                    <S.ProfileImage>
+                        <RenderIcon
+                            family="materialIcons"
+                            icon="account-circle"
+                            fontSize={rem(4)}
+                            color="primary"
+                        />
+                    </S.ProfileImage>
+                    <S.ProfileInfo>
+                        <S.UserName>{userName}</S.UserName>
+                        <S.DateJoined>{t('Joined')} {dateJoined}</S.DateJoined>
+                        <S.QRButton>
+                            <RenderIcon
+                                family="materialCommunity"
+                                icon="qrcode"
+                                fontSize={rem(1.5)}
+                                color="primary"
+                            />
+                            <S.QRButtonText>{t('View my QR code')}</S.QRButtonText>
+                        </S.QRButton>
+                    </S.ProfileInfo>
+                </S.ProfileCardContent>
+                <S.EditIconBtn>
+                    <RenderIcon
+                        family="materialIcons"
+                        icon="edit"
+                        fontSize={rem(1.5)}
+                        color="white"
+                    />
+                </S.EditIconBtn>
+            </S.ProfileCard>
+
+            <S.SectionTitle>{t('Account Details')}</S.SectionTitle>
+            <S.LightCard>
+                <S.ItemRow>
+                    <S.ItemTextCol>
+                        <S.ItemLabel>{t('Name')}</S.ItemLabel>
+                        <S.ItemValue>{userName}</S.ItemValue>
+                    </S.ItemTextCol>
+                    <S.EditIconBtnLight>
+                        <RenderIcon family="materialIcons" icon="edit" fontSize={rem(1.25)} color="primary" />
+                    </S.EditIconBtnLight>
+                </S.ItemRow>
+                <S.ItemRow>
+                    <S.ItemTextCol>
+                        <S.ItemLabel>{t('Gender')}</S.ItemLabel>
+                        <S.ItemValue>{gender}</S.ItemValue>
+                    </S.ItemTextCol>
+                    <S.EditIconBtnLight>
+                        <RenderIcon family="materialIcons" icon="edit" fontSize={rem(1.25)} color="primary" />
+                    </S.EditIconBtnLight>
+                </S.ItemRow>
+                <S.ItemRow>
+                    <S.ItemTextCol>
+                        <S.ItemLabel>{t('Date of birth')}</S.ItemLabel>
+                        <S.ItemValue>{dob}</S.ItemValue>
+                    </S.ItemTextCol>
+                    <S.EditIconBtnLight>
+                        <RenderIcon family="materialIcons" icon="edit" fontSize={rem(1.25)} color="primary" />
+                    </S.EditIconBtnLight>
+                </S.ItemRow>
+            </S.LightCard>
+
+            <S.SectionTitle>{t('Contact Data')}</S.SectionTitle>
+            <S.LightCard>
+                <S.ItemRow>
+                    <S.ItemTextCol>
+                        <S.ItemLabel>{t('Email address')}</S.ItemLabel>
+                        <S.ItemValue>{email}</S.ItemValue>
+                    </S.ItemTextCol>
+                    <S.EditIconBtnLight>
+                        <RenderIcon family="materialIcons" icon="edit" fontSize={rem(1.25)} color="primary" />
+                    </S.EditIconBtnLight>
+                </S.ItemRow>
+                <S.ItemRow>
+                    <S.ItemTextCol>
+                        <S.ItemLabel>{t('Phone number')}</S.ItemLabel>
+                        <S.ItemValue>{phone}</S.ItemValue>
+                    </S.ItemTextCol>
+                    <S.EditIconBtnLight>
+                        <RenderIcon family="materialIcons" icon="edit" fontSize={rem(1.25)} color="primary" />
+                    </S.EditIconBtnLight>
+                </S.ItemRow>
+                <S.ItemRow>
+                    <S.ItemTextCol>
+                        <S.ItemLabel>{t('Address')}</S.ItemLabel>
+                        <S.ItemValue>{address}</S.ItemValue>
+                    </S.ItemTextCol>
+                    <S.EditIconBtnLight>
+                        <RenderIcon family="materialIcons" icon="edit" fontSize={rem(1.25)} color="primary" />
+                    </S.EditIconBtnLight>
+                </S.ItemRow>
+            </S.LightCard>
+
+            <S.DeleteButton>
+                <S.DeleteButtonText>{t('Delete My Account')}</S.DeleteButtonText>
+            </S.DeleteButton>
+
+            <S.ActionButton>
+                <S.ActionButtonText>{t('Change password')}</S.ActionButtonText>
+            </S.ActionButton>
+            <S.ActionButton>
+                <S.ActionButtonText>{t('Sign out')}</S.ActionButtonText>
+            </S.ActionButton>
+        </S.Scroll>
+    );
+};
+
+export default Account;
