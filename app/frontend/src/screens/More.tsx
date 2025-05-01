664820fe09104ed353ed21e1cae495b24c27d1ea
so-mb <somtochukwumbuko@gmail.com>
2025-05-01 13:54:50 +0200

    Add More screen with styled components for settings and support options; implement navigation actions for user interactions

--- app/frontend/src/screens/More.tsx
+++ app/frontend/src/screens/More.tsx
@@ -1,14 +1,73 @@
 import React from 'react';
-import * as S from './Home.styles';
+import { Linking } from 'react-native';
+import RenderIcon from '../components/RenderIcon';
+import companyInfo from '../utils/companyInfo.json';
+import * as S from './More.styles';
+
+const options = [
+    {
+        section: 'App settings',
+        content: [
+            {
+                iconFamily: 'ionIcons',
+                icon: 'language',
+                label: 'Language',
+                onPress: () => {/* TODO: navigate to language settings */ }
+            },
+            {
+                iconFamily: 'materialIcons',
+                icon: 'perm-device-info',
+                label: 'Device permissions',
+                onPress: () => {/* TODO: navigate or open settings */ }
+            },
+        ],
+    },
+    {
+        section: 'More info and support',
+        content: [
+            {
+                iconFamily: 'materialIcons',
+                icon: 'email',
+                label: 'Email us',
+                onPress: () => Linking.openURL(`mailto:${companyInfo.email}`)
+            },
+            {
+                iconFamily: 'materialIcons',
+                icon: 'phone',
+                label: 'Call us',
+                onPress: () => Linking.openURL(`tel:${companyInfo.phone}`)
+            },
+            {
+                iconFamily: 'materialCommunity',
+                icon: 'web',
+                label: 'Visit our website',
+                onPress: () => Linking.openURL(companyInfo.website)
+            },
+        ],
+    },
+];
 
 export const More: React.FC = () => {
-
     return (
-        <S.Root>
-            <S.Title>
-                More Page
-            </S.Title>
-        </S.Root>
+        <S.Container>
+            {options.map((section, idx) => (
+                <React.Fragment key={section.section}>
+                    {idx !== 0 && <S.SectionDivider />}
+                    <S.SectionHeader>{section.section}</S.SectionHeader>
+                    {section.content.map((opt) => (
+                        <S.OptionButton key={opt.label} onPress={opt.onPress}>
+                            <S.OptionIconTextWrapper>
+                                <RenderIcon family={opt.iconFamily as any} icon={opt.icon} fontSize={22} color="primary" />
+                                <S.OptionText>{opt.label}</S.OptionText>
+                            </S.OptionIconTextWrapper>
+                            <S.OptionArrow>
+                                <RenderIcon family="materialIcons" icon="chevron-right" fontSize={28} color="primary" />
+                            </S.OptionArrow>
+                        </S.OptionButton>
+                    ))}
+                </React.Fragment>
+            ))}
+        </S.Container>
     );
 };
 
