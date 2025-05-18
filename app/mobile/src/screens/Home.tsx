feb394cd6e7a6d35db726970a487590d6ca5e7a8
so-mb <somtochukwumbuko@gmail.com>
2025-05-18 17:11:35 +0200

    make Home.tsx DRY

--- app/mobile/src/screens/Home.tsx
+++ app/mobile/src/screens/Home.tsx
@@ -1,23 +1,39 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
 import * as S from './Home.styles';
 import RenderLottie from '../components/RenderLottie';
 import RenderIcon from '../components/RenderIcon';
 import { rem } from '../utils/Responsive';
 import Api from '../api/Client';
-import { useEffect } from 'react';
 
+type GridItem = {
+  key: string;
+  label: string;
+  family: string;
+  icon: string;
+  route?: string;
+};
 
-export const Home: React.FC = () => {
+const GRID_ITEMS: GridItem[] = [
+  { key: 'overviews', label: 'Overviews', family: 'materialCommunity', icon: 'format-list-checkbox' },
+  { key: 'discover', label: 'Discover', family: 'octicons', icon: 'light-bulb', route: 'Discover' },
+  { key: 'news', label: 'News', family: 'fontAwesome', icon: 'newspaper-o' },
+  { key: 'contactUs', label: 'Contact Us', family: 'materialCommunity', icon: 'email-send-outline' },
+];
+
+export const Home: React.FC<any> = ({ navigation }) => {
   const { t } = useTranslation();
-
-  const [userName, setUserName] = useState<string | null>(null);
+  const [userName, setUserName] = useState<string>('');
 
   useEffect(() => {
     Api.get('users/me/')
-      .then((res: any) => setUserName(res.data.full_name))
-      .catch((err: any) => console.error('Could not load profile', err));
+      .then(res => setUserName(res.data.full_name))
+      .catch(err => console.error('Could not load profile', err));
   }, []);
+
+  const handlePress = (route?: string) => {
+    if (route) navigation.navigate(route);
+  };
 
   return (
     <S.Root>
@@ -25,60 +41,30 @@ export const Home: React.FC = () => {
         <S.HeaderRow>
           <S.Greeting>{t('hello')}</S.Greeting>
           <S.Lottie>
-            <RenderLottie
-              name="homeHello"
-              startFrame={0}
-              endFrame={150}
-            />
+            <RenderLottie name="homeHello" startFrame={0} endFrame={150} />
           </S.Lottie>
         </S.HeaderRow>
         <S.UserName>{userName}</S.UserName>
       </S.Header>
+
       <S.Grid>
-        <S.Box>
-          <S.BoxIcon>
-            <RenderIcon
-              family="materialCommunity"
-              icon="format-list-checkbox"
-              fontSize={rem(2.25)}
-              color="primary"
-            />
-          </S.BoxIcon>
-          <S.BoxLabel>Overviews</S.BoxLabel>
-        </S.Box>
-        <S.Box>
-          <S.BoxIcon>
-            <RenderIcon
-              family="octicons"
-              icon="light-bulb"
-              fontSize={rem(2.25)}
-              color="primary"
-            />
-          </S.BoxIcon>
-          <S.BoxLabel>Discover</S.BoxLabel>
-        </S.Box>
-        <S.Box>
-          <S.BoxIcon>
-            <RenderIcon
-              family="fontAwesome"
-              icon="newspaper-o"
-              fontSize={rem(2.25)}
-              color="primary"
-            />
-          </S.BoxIcon>
-          <S.BoxLabel>News</S.BoxLabel>
-        </S.Box>
-        <S.Box>
-          <S.BoxIcon>
-            <RenderIcon
-              family="materialCommunity"
-              icon="email-send-outline"
-              fontSize={rem(2.25)}
-              color="primary"
-            />
-          </S.BoxIcon>
-          <S.BoxLabel>Contact Us</S.BoxLabel>
-        </S.Box>
+        {GRID_ITEMS.map(item => (
+          <S.Box
+            key={item.key}
+            onPress={() => handlePress(item.route)}
+            disabled={!item.route}
+          >
+            <S.BoxIcon>
+              <RenderIcon
+                family={item.family as any}
+                icon={item.icon}
+                fontSize={rem(2.25)}
+                color="primary"
+              />
+            </S.BoxIcon>
+            <S.BoxLabel>{item.label}</S.BoxLabel>
+          </S.Box>
+        ))}
       </S.Grid>
     </S.Root>
   );
