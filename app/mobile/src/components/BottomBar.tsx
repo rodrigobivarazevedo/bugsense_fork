28366f1a753d84d3ddbcbf676c27b5964c1100a5
so-mb <somtochukwumbuko@gmail.com>
2025-05-18 17:05:01 +0200

    Refactor BottomBar component to simplify navigation handling
    - Removed NavigationRoute wrapper for each tab in the BottomBar component to streamline the structure.
    - Updated state management for active tab to use string type instead of RouteName.
    - Enhanced tab change handling to navigate directly using the navigation prop.
    - Improved code readability and maintainability by reducing unnecessary complexity.

--- app/mobile/src/components/BottomBar.tsx
+++ app/mobile/src/components/BottomBar.tsx
@@ -2,86 +2,81 @@ import React, { useState, useEffect } from 'react';
 import { TouchableOpacity } from 'react-native';
 import * as S from './BottomBar.styles';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
-import NavigationRoute, { RouteName } from './NavigationRoute';
-import { useNavigationState } from '@react-navigation/native';
+import { useNavigation, useNavigationState } from '@react-navigation/native';
 import RenderIcon from './RenderIcon';
 
 const BottomBar: React.FC = () => {
     const insets = useSafeAreaInsets();
-    const [activeTab, setActiveTab] = useState<RouteName>('home');
+    const [activeTab, setActiveTab] = useState<string>('home');
+    const navigation = useNavigation();
     const navigationState = useNavigationState(state => state);
 
     useEffect(() => {
         if (navigationState && navigationState.routes.length > 0) {
-            const currentRoute = navigationState.routes[navigationState.index].name.toLowerCase() as RouteName;
+            const currentRoute = navigationState.routes[navigationState.index].name.toLowerCase() as string;
             if (currentRoute === 'home' || currentRoute === 'scan' ||
                 currentRoute === 'results' || currentRoute === 'more') {
                 setActiveTab(currentRoute);
+            } else {
+                setActiveTab("");
             }
         }
     }, [navigationState]);
 
-    const handleTabChange = (tab: RouteName) => {
+    const handleTabChange = (tab: string) => {
         setActiveTab(tab);
+        navigation.navigate(tab.charAt(0).toUpperCase() + tab.slice(1) as never);
     };
 
     return (
         <S.Container insets={insets}>
-            <NavigationRoute route="home">
-                <TouchableOpacity onPress={() => handleTabChange('home')}>
-                    <S.IconWrapper>
-                        <RenderIcon
-                            family="foundation"
-                            icon="home"
-                            fontSize={24}
-                            color={activeTab === 'home' ? "primary" : "themeGray"}
-                        />
-                        <S.Label isActive={activeTab === 'home'}>Home</S.Label>
-                    </S.IconWrapper>
-                </TouchableOpacity>
-            </NavigationRoute>
+            <TouchableOpacity onPress={() => handleTabChange('home')}>
+                <S.IconWrapper>
+                    <RenderIcon
+                        family="foundation"
+                        icon="home"
+                        fontSize={24}
+                        color={activeTab === 'home' ? "primary" : "themeGray"}
+                    />
+                    <S.Label isActive={activeTab === 'home'}>Home</S.Label>
+                </S.IconWrapper>
+            </TouchableOpacity>
 
-            <NavigationRoute route="scan">
-                <TouchableOpacity onPress={() => handleTabChange('scan')}>
-                    <S.IconWrapper>
-                        <RenderIcon
-                            family="entypo"
-                            icon="camera"
-                            fontSize={24}
-                            color={activeTab === 'scan' ? "primary" : "themeGray"}
-                        />
-                        <S.Label isActive={activeTab === 'scan'}>Scan</S.Label>
-                    </S.IconWrapper>
-                </TouchableOpacity>
-            </NavigationRoute>
+            <TouchableOpacity onPress={() => handleTabChange('scan')}>
+                <S.IconWrapper>
+                    <RenderIcon
+                        family="entypo"
+                        icon="camera"
+                        fontSize={24}
+                        color={activeTab === 'scan' ? "primary" : "themeGray"}
+                    />
+                    <S.Label isActive={activeTab === 'scan'}>Scan</S.Label>
+                </S.IconWrapper>
+            </TouchableOpacity>
 
-            <NavigationRoute route="results">
-                <TouchableOpacity onPress={() => handleTabChange('results')}>
-                    <S.IconWrapper>
-                        <RenderIcon
-                            family="foundation"
-                            icon="results"
-                            fontSize={24}
-                            color={activeTab === 'results' ? "primary" : "themeGray"}
-                        />
-                        <S.Label isActive={activeTab === 'results'}>Results</S.Label>
-                    </S.IconWrapper>
-                </TouchableOpacity>
-            </NavigationRoute>
+            <TouchableOpacity onPress={() => handleTabChange('results')}>
+                <S.IconWrapper>
+                    <RenderIcon
+                        family="foundation"
+                        icon="results"
+                        fontSize={24}
+                        color={activeTab === 'results' ? "primary" : "themeGray"}
+                    />
+                    <S.Label isActive={activeTab === 'results'}>Results</S.Label>
+                </S.IconWrapper>
+            </TouchableOpacity>
 
-            <NavigationRoute route="more">
-                <TouchableOpacity onPress={() => handleTabChange('more')}>
-                    <S.IconWrapper>
-                        <RenderIcon
-                            family="feather"
-                            icon="more-horizontal"
-                            fontSize={24}
-                            color={activeTab === 'more' ? "primary" : "themeGray"}
-                        />
-                        <S.Label isActive={activeTab === 'more'}>More</S.Label>
-                    </S.IconWrapper>
-                </TouchableOpacity>
-            </NavigationRoute>
+            <TouchableOpacity onPress={() => handleTabChange('more')}>
+                <S.IconWrapper>
+                    <RenderIcon
+                        family="feather"
+                        icon="more-horizontal"
+                        fontSize={24}
+                        color={activeTab === 'more' ? "primary" : "themeGray"}
+                    />
+                    <S.Label isActive={activeTab === 'more'}>More</S.Label>
+                </S.IconWrapper>
+            </TouchableOpacity>
         </S.Container>
     );
 };
