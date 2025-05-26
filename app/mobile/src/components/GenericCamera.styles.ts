7aae3272036358146e74342ce307f4d27eb8091a
so-mb <somtochukwumbuko@gmail.com>
2025-05-26 22:26:41 +0200

    Refactor GenericCamera component to enhance UI and functionality: add flash and icons

--- app/mobile/src/components/GenericCamera.styles.ts
+++ app/mobile/src/components/GenericCamera.styles.ts
@@ -20,12 +20,11 @@ export const StyledCamera = styled(CameraView)`
   flex: 1;
 `;
 
-export const ButtonContainer = styled.View`
+export const TopControlsContainer = styled.View`
   position: absolute;
-  bottom: ${rem(1.25)}px;
-  width: 100%;
-  flex-direction: row;
-  justify-content: space-evenly;
+  top: ${rem(1)}px;
+  left: ${rem(0.75)}px;
+  gap: ${rem(0.5)}px;
 `;
 
 export const Button = styled(TouchableOpacity)`
@@ -34,7 +33,36 @@ export const Button = styled(TouchableOpacity)`
   border-radius: ${rem(0.5)}px;
 `;
 
+export const BottomControlsContainer = styled.View`
+  position: absolute;
+  bottom: ${rem(1.5)}px;
+  width: 100%;
+  align-items: center;
+`;
+
+export const ControlButton = styled(TouchableOpacity)`
+  background-color: rgba(0, 0, 0, 0.35);
+  padding: ${rem(0.75)}px ${rem(1)}px;
+  border-radius: ${rem(0.5)}px;
+`;
+
+export const SnapButton = styled(TouchableOpacity)`
+  width: ${rem(4)}px;
+  height: ${rem(4)}px;
+  border-radius: ${rem(2)}px;
+  border: ${rem(0.25)}px solid ${themeColors.white};
+  align-items: center;
+  justify-content: center;
+`;
+
+export const SnapButtonInner = styled.View`
+  width: ${rem(3)}px;
+  height: ${rem(3)}px;
+  border-radius: ${rem(1.5)}px;
+  background-color: ${themeColors.white};
+`;
+
 export const ButtonText = styled.Text`
-  font-size: ${rem(1)}px;
+  font-size: ${rem(0.875)}px;
   color: ${themeColors.black};
 `;
--- app/mobile/src/components/GenericCamera.tsx
+++ app/mobile/src/components/GenericCamera.tsx
@@ -3,12 +3,17 @@ import {
   CameraView,
   useCameraPermissions,
   CameraType,
+  FlashMode,
 } from 'expo-camera';
+import RenderIcon from './RenderIcon';
+import { Platform } from 'react-native';
+import { themeColors } from '../theme/GlobalTheme';
 import * as S from './GenericCamera.styles';
 
 export const GenericCamera = () => {
   const [permission, requestPermission] = useCameraPermissions();
   const [facing, setFacing] = useState<CameraType>('back');
+  const [flash, setFlash] = useState<FlashMode>('off');
   const cameraRef = useRef<CameraView>(null);
 
   if (!permission) {
@@ -40,27 +45,64 @@ export const GenericCamera = () => {
     console.log(photo?.uri);
   };
 
+  const toggleFlash = () => {
+    setFlash(flash === 'off' ? 'on' : flash === 'on' ? 'auto' : 'off');
+  };
+
   return (
     <S.Root>
       <S.StyledCamera
         ref={cameraRef}
         facing={facing}
+        flashMode={flash}
       >
-        <S.ButtonContainer>
-          <S.Button onPress={snapPicture}>
-            <S.ButtonText>Snap</S.ButtonText>
-          </S.Button>
-          <S.Button
-            onPress={() =>
-              setFacing(facing === 'back' ? 'front' : 'back')
-            }
+        <S.TopControlsContainer>
+          <S.ControlButton onPress={toggleFlash}>
+            {flash === 'on' ? (
+              <RenderIcon
+                family='materialIcons'
+                icon="flash-on"
+                fontSize={24}
+                color={themeColors.white}
+              />
+            ) : flash === 'auto' ? (
+              <RenderIcon
+                family='materialIcons'
+                icon="flash-auto"
+                fontSize={24}
+                color={themeColors.white}
+              />
+            ) : (
+              <RenderIcon
+                family='materialIcons'
+                icon="flash-off"
+                fontSize={24}
+                color={themeColors.white}
+              />
+            )}
+          </S.ControlButton>
+          <S.ControlButton
+            onPress={() => setFacing(facing === 'back' ? 'front' : 'back')}
           >
-            <S.ButtonText>Flip</S.ButtonText>
-          </S.Button>
-        </S.ButtonContainer>
+            <RenderIcon
+              family='materialIcons'
+              icon={Platform.OS === 'android' ? 'flip-camera-android' : 'flip-camera-ios'}
+              fontSize={24}
+              color={themeColors.white}
+            />
+          </S.ControlButton>
+        </S.TopControlsContainer>
+        <S.BottomControlsContainer>
+          <S.SnapButton onPress={snapPicture}>
+            <S.SnapButtonInner />
+          </S.SnapButton>
+        </S.BottomControlsContainer>
       </S.StyledCamera>
     </S.Root>
   );
 }
 
 export default GenericCamera;
+
+// TODO: Check out Barcode feature
+// For more features, check out CameraView.tsx, Camera.types.ts, etc.
