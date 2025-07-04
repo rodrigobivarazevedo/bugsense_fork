33e403be58560a6c54e88d01d4e32e2b81861171
so-mb <somtochukwumbuko@gmail.com>
2025-07-04 19:40:48 +0200

    Replace secret key generation method in start_app.sh with generic python method

--- app/start_app.sh
+++ app/start_app.sh
@@ -20,7 +20,7 @@ show_help() {
 }
 
 generate_secret_key() {
-  python3 -c "from django.core.management.utils import get_random_secret_key; print(get_random_secret_key())"
+  python3 -c "import secrets; print(secrets.token_urlsafe(50))"
 }
 
 export HOST_IP=$(ifconfig en0 \
