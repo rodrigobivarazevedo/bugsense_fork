d347cd6848af4e90cda14298d6e1423a4f064c7a
rodrigobivarazevedo <rodrigobivarazevedo@gmail.com>
2025-07-08 14:28:52 +0200

    fix start up script

--- app/start_app.sh
+++ app/start_app.sh
@@ -23,9 +23,15 @@ generate_secret_key() {
   python3 -c "import secrets; print(secrets.token_urlsafe(50))"
 }
 
-export HOST_IP=$(ifconfig en0 \
-  | awk '/inet / && !/127/ {print $2; exit}')
+get_host_ip() {
+  ifconfig en0 | awk '/inet / && $2 != "127.0.0.1" {print $2; exit}'
+}
 
+
+# export HOST_IP=$(ifconfig en0 \
+#   | awk '/inet / && !/127/ {print $2; exit}')
+
+export HOST_IP=$(get_host_ip)
 
 # Generate new keys
 ML_API_KEY=$(generate_secret_key)
@@ -45,17 +51,23 @@ else
     echo "ML_API_KEY=${ML_API_KEY}" >> ./ai-api/.env
 fi
 
+if grep -q '^HOST_IP=' ./ai-api/.env; then
+    sed -i '' "s/^HOST_IP=.*/HOST_IP=${HOST_IP}/" ./ai-api/.env  # macOS version
+else
+    echo "HOST_IP=${HOST_IP}" >> ./ai-api/.env
+fi
+
 
 # core-api
 
 if [ -f .env ]; then
   if ! grep -q "DJANGO_SECRET_KEY" .env; then
     echo "Adding DJANGO_SECRET_KEY to existing .env file..."
-    echo "DJANGO_SECRET_KEY=${ML_API_KEY}" >> .env
+    echo "DJANGO_SECRET_KEY=${DJANGO_SECRET_KEY}" >> .env
   fi
   if ! grep -q "ML_API_KEY" .env; then
     echo "Adding ML_API_KEY to existing .env file..."
-    echo "ML_API_KEY=${DJANGO_API_KEY}" >> .env
+    echo "ML_API_KEY=${ML_API_KEY}" >> .env
   fi
 else
   echo "Creating new .env file..."
